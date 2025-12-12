86f994271c872ed77410bdcb6680f457ca936ae1
Kethiteddy Govardhan <139302088+Govardhan8378@users.noreply.github.com>
2025-12-12 19:46:53 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,7 @@
+
+name = input("Enter your name? ")
+age = input("Enter your age? ")
+hobby = input("Enter your hobby? ")
+
+print(f"\n🎉  Welcome {name}! 🎉")
+print(f"You are {age} years old and {hobby}.")
